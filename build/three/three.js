151ef57ea4da0025d508b51f6165706bd5786b3a
minhoyoo <minhoyoo1899@gmai.com>
2022-12-11 01:16:37 +0900

    Uncaught SyntaxError: The requested module GLTFLoader does not provide an export named default

--- build/three/three.js
+++ build/three/three.js
@@ -1,6 +1,7 @@
 // import * as THREE from 'three';
 // const THREE = require('three');
 import * as THREE from "three";
+import GLTFLoader from "GLTFLoader";
 // import * as THREE from '../../node_modules/three/build/three.module.js';
 // import GLTFLoader from '../../node_modules/three-gltf-loader/index.js';
 // import GLTFLoader from "../../node_modules/three/examples/jsm/loaders/GLTFLoader.js";
