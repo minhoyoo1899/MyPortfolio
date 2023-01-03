8503d224666d4ac85d84f66fe010136277f21c0d
MinhoYoo <minhoyoo1899@gmail.com>
2023-01-03 16:45:59 +0900

    http 모듈에서 three.js 띄우기 성공!

--- server/http.ts
+++ server/http.ts
@@ -3,19 +3,36 @@ import * as fs from "fs";
 
 const index = fs.readFileSync('../public/index.html', 'utf-8');
 // const threeJS = fs.readFileSync('../build/three/three.js', 'utf-8');
-const DNS = http.createServer((req: any, res: any) => {
+const threeJS = fs.readFileSync('../build/three/three.js', 'utf-8');
+const threeModule = fs.readFileSync('../node_modules/three/build/three.module.js', 'utf-8');
+const GLTFLoader = fs.readFileSync('../node_modules/three/examples/jsm/loaders/GLTFLoader.js', 'utf-8');
+const httpServer = http.createServer((req: any, res: any) => {
   let url = req.url;
+  console.log(url)
   switch (req.method) {
     case "GET":
       if (url === "/") {
         res.writeHead(200, { 'Content-Type': 'text:html; charset=UTF-8;' });
-        res.write("test");
+        res.write(index);
         res.end();
       // } else if (url === "/three.js") {
       //   res.writeHead(200, { 'Content-Type': 'text:javascript; charset=UTF-8;' });
       //   res.write(threeJS);
       //   res.end();
+      } else if (url === "/build/three/three.js") {
+        res.writeHead(200, { 'Content-Type': 'text/javascript; charset=UTF-8;' });
+        res.write(threeJS);
+        res.end();
+      } else if (url === "/node_modules/three/build/three.module.js") {
+        res.writeHead(200, { 'Content-Type': 'text/javascript; charset=UTF-8;' });
+        res.write(threeModule);
+        res.end();
+      } else if (url === "/node_modules/three/examples/jsm/loaders/GLTFLoader.js") {
+        res.writeHead(200, { 'Content-Type': 'text/javascript; charset=UTF-8;' });
+        res.write(GLTFLoader);
+        res.end();
       }
+
     case "POST":
       console.log("POST");
       break;
@@ -40,4 +57,4 @@ const DNS = http.createServer((req: any, res: any) => {
   }
 });
 
-DNS.listen(8282);
+httpServer.listen(8282);
