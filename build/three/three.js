5efa6033f7eeca0a4fb082a26af6ee8fb60b9f70
MinhoYoo <minhoyoo1899@gmail.com>
2023-01-03 15:59:57 +0900

    three.js 예제 파일 추가

--- build/three/three.js
+++ build/three/three.js
@@ -1,75 +1,42 @@
-// import * as THREE from 'three';
-// const THREE = require('three');
 import * as THREE from "three";
 import { GLTFLoader } from "GLTFLoader";
-// import * as THREE from '../../node_modules/three/build/three.module.js';
-// import GLTFLoader from '../../node_modules/three-gltf-loader/index.js';
-// import GLTFLoader from "../../node_modules/three/examples/jsm/loaders/GLTFLoader.js";
 
-// import { GLTFLoader }  from "three/examples/jsm/loaders/GLTFLoader.js";
-console.log(THREE);
-// console.log(GLTFLoader);
+function init() {
+	const scene = new THREE.Scene();
+	const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+	const renderer = new THREE.WebGLRenderer();
+	renderer.setClearColor(0xEEEEEE); // setClearColorHex 함수 없음
+	renderer.setSize(window.innerWidth, window.innerHeight);
 
-// init
-// const loader = new GLTFLoader();
+	const axes = new THREE.AxesHelper(20); // axisHelper함수 없음 아마도 업데이트 된 듯?	
+	scene.add(axes);
 
-// loader.load( '/src/testobj/testobj_squre001.glb', function ( gltf ) {
+	const cube = new THREE.Mesh(cubeGeometry, cubeMeterial);
 
-// 	scene.add( gltf.scene );
+	cube.position.x = -4;
+	cube.position.y = 3;
+	cube.position.z = 0;
 
-// }, undefined, function ( error ) {
+	scene.add(cube);
 
-// 	console.error( error );
+	const sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
+	const sphereMateral = new THREE.MeshBasicMaterial({ color: 0x777ff, wireframe: true });
+	const sphere = new THREE.Mesh(sphereGeometry, sphereMateral);
 
-// } );
+	sphere.position.x = 20;
+	sphere.position.y = 4;
+	sphere.position.z = 2;
 
-// loader.load('/src/testobj/testobj_squre001.glb', ( gltf ) => {
-//         // called when the resource is loaded
-//         scene.add( gltf.scene );
-//     }, ( xhr ) => {
-//         // called while loading is progressing
-//         console.log( `${( xhr.loaded / xhr.total * 100 )}% loaded` );
-//     }, ( error ) => {
-//         // called when loading has errors
-//         console.error( 'An error happened', error );
-//     },
-// );
+	scene.add(sphere);
 
+	camera.position.x = -30;
+	camera.position.y = 40;
+	camera.position.z = 30;
+	camera.lookAt(scene.position);
 
-const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-camera.position.z = 1;
+	const root = document.getElementById("root");
+	root.appendChild(renderer.domElement);
+	renderer.render(scene, camera);
+};
 
-const scene = new THREE.Scene();
-
-const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-const material = new THREE.MeshNormalMaterial();
-
-const mesh = new THREE.Mesh( geometry, material );
-scene.add( mesh );
-
-const renderer = new THREE.WebGLRenderer( { antialias: true } );
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animation );
-document.body.appendChild( renderer.domElement );
-
-
-const GLTFloader = new GLTFLoader();
-console.log(GLTFloader);
-
-GLTFloader.load("../../src/testobj/testobj_squre001.glb", (gltf) => { 
-	console.log(gltf);
-	scene.add(gltf.scene);
-	console.log(gltf.scene);
-});
-
-// animation
-
-function animation(time) {
-	// requestAnimationFrame(animate);
-
-	mesh.rotation.x = time / 2000;
-	mesh.rotation.y = time / 1000;
-
-	renderer.render( scene, camera );
-
-}
+window.onload = init;
